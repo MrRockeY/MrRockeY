006e3f60e9bb9f41c04b14d02a1fc03e70c6f42a
MrRockeY <mrrockeyx@gmail.com>
2025-03-20 08:50:36 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mrrockey/", // ✅ Add this line (Set it to your repository name)
   server: {
     host: "::",
     port: 8080,
